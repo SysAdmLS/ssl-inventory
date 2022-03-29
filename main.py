03feb937f2a0807645584af149e041ccb3a5b90a
Lucas Stricker <lucas.stricker@jena.de>
2022-03-29 18:15:05 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,153 @@
+import json
+import socket
+import logging
+from timeit import default_timer as timer
+
+from cryptography import x509
+from elasticsearch import Elasticsearch
+from libnmap.parser import NmapParser
+from libnmap.process import NmapProcess
+
+# config
+es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])  # elasticsearch connection
+
+
+class Cert:
+    """Class to handle the certificate"""
+
+    def __init__(self, pem: str):
+        self.certobj = x509.load_pem_x509_certificate(str.encode(pem))
+        self.notvalidafter = None
+        self.issuer = None
+        self.cn = None
+        self.san = None
+
+        self.__parse()
+
+    def __parse(self):
+        """Parses the cert fields/data"""
+        self.notvalidafter = self.certobj.not_valid_after
+
+        # catches certs not having a valid subject
+        if self.certobj.subject.get_attributes_for_oid(x509.OID_COMMON_NAME):
+            self.cn = self.certobj.subject.get_attributes_for_oid(x509.OID_COMMON_NAME)[
+                0].value.strip()
+
+        # catches certs not having a valid issuer
+        if self.certobj.issuer.get_attributes_for_oid(x509.OID_COMMON_NAME):
+            self.issuer = self.certobj.issuer.get_attributes_for_oid(x509.OID_COMMON_NAME)[
+                0].value.strip()
+        # catches certs not having SAN's
+        try:
+            self.san = self.certobj.extensions.get_extension_for_class(
+                x509.SubjectAlternativeName).value.get_values_for_type(x509.DNSName)
+        except:
+            self.san = ''
+
+
+class ScanresultMasscan:
+    """Class for importing Masscan results in json format to elasticsearch"""
+
+    def __init__(self, jsonfile: str, indexname: str):
+        self.jsonfile = jsonfile
+        with open(self.jsonfile) as f:
+            self.data = json.load(f)
+
+        self.es_index = indexname
+        self.__parse()
+
+    def __index(self, entry):
+        """Function to write the datapoint to elasticsearch"""
+        es.index(index=self.es_index, id=entry['ip'] + ':' + str(entry['port']), document=entry)
+
+    def parse(self):
+        """Parse the masscan json"""
+        for result in self.data:
+            for port in result['ports']:
+
+                try:
+                    if port['service']['name'] == 'X509':
+                        certificate = Cert(
+                            '-----BEGIN CERTIFICATE-----\n' + port['service']['banner'] + '\n-----END CERTIFICATE-----')
+                        print(result)
+
+                        dataentry = {
+                            'timestamp': result['timestamp'],
+                            'ip': result['ip'],
+                            'hostname': '',
+                            'port': int(port['port']),
+                            'notvalidafter': certificate.notvalidafter,
+                            'subject': certificate.cn,
+                            'issuer': certificate.issuer,
+                            'SANs': certificate.san
+
+                        }
+
+                        try:
+                            dataentry['hostname'] = socket.gethostbyaddr(result['ip'])[0]
+                        except socket.herror as e:
+                            logging.warning(e)
+
+                        print(dataentry)
+                        self.__index(dataentry)
+                except KeyError:
+                    pass
+                except ValueError:
+                    logging.warning(result['ip'] + ' failed')
+
+
+class ScanresultNmap():
+    def __init__(self, indexname: str):
+        self.es_index = indexname
+
+    def __index(self, entry):
+        """Function to write the datapoint to elasticsearch"""
+        es.index(index=self.es_index, id=entry['ip'] + ':' + str(entry['port']), document=entry)
+
+    def parsefromfile(self, xmlfile: str):
+        """Function to parse nmap xmlfile output to elasticsearch"""
+        nmap_report = NmapParser.parse_fromfile(xmlfile)
+        self.parse(nmap_report)
+
+    def parse(self, nmap_report):
+        for host in nmap_report.hosts:
+            for service in host.services:
+                if service.scripts_results:
+                    certificate = Cert(service.scripts_results[0]['elements']['pem'])
+
+                    dataentry = {
+                        'timestamp': host.starttime,
+                        'ip': host.ipv4,
+                        'hostname': '',
+                        'port': int(service.port),
+                        'notvalidafter': certificate.notvalidafter,
+                        'subject': certificate.cn,
+                        'issuer': certificate.issuer,
+                        'SANs': certificate.san
+
+                    }
+                    if host.hostnames:
+                        dataentry['hostname'] = host.hostnames[0]
+
+                    print(dataentry)
+                    self.__index(dataentry)
+
+    def masscantonmap(self, masscan_json: str):
+        with open(masscan_json) as f:
+            data = json.load(f)
+        for result in data:
+            for port in result['ports']:
+                print(f"{result['ip']} : {port['port']}")
+                nm = NmapProcess(result['ip'], options=f"-p{port['port']} --script ssl-cert --script-timeout 2")
+                nm.run()
+                self.parse(NmapParser.parse(nm.stdout))
+
+
+if __name__ == '__main__':
+    # ScanresultMasscan('test.json', 'test')
+    # ScanresultMasscan('test2.json', 'test')
+    # ScanresultNmap('test').parsefromfile('test.xml')
+    start = timer()
+    ScanresultNmap('test').masscantonmap('test.json')
+    end = timer()
+    print(end - start)
---
+++ readme.md
@@ -0,0 +1,17 @@
+# SSL Certificate Inventory (POC)
+
+This is a POC on how to inventorize used SSL Certificates for example in an enterprise environment. The idea was to
+use  [masscan](https://github.com/robertdavidgraham/masscan) as the primary network scanner utilizing its huge speed advantage over Nmap and importing the results into an Elasticsearch-Kibana Stack for exploring and generating alerts.  
+
+However, there are some **limitations** to that: 
+1. Masscan has limited support for [grabbing](https://github.com/robertdavidgraham/masscan#banner-checking) the SSL Certificate from services other than HTTPS/SMTP/IMAP.
+2. Masscan uses a **hardcoded** TLS 1.1 "Client Hello" packet (see [here](https://github.com/robertdavidgraham/masscan/blob/144c527ed55275ee9fbb80bb14fbb5e3fcff3b7e/src/proto-ssl.c#L1059)). This leads to problems retrieving the SSL Certificate from servers not supporting TLS 1.1, which is deprecated [since March 2021](https://datatracker.ietf.org/doc/rfc8996/).
+3. Masscan might not be available on the users' system.
+
+To combat these limitations I implemented several ways for importing and generating scan results.
+
+I also compiled my own masscan binary, replacing the TLS 1.1 packet with TLS 1.2, as this increased coverage and reduced connection errors significantly when grabbing for Certificates. The binary is included in this repo. Compiling your own TLS 1.2 masscan version is as easy as capturing a TLS 1.2 handshake (e.g. ``curl --tlsv1.2 https://10.X.X.X``   ) with wireshark and inserting the packet [here](https://github.com/robertdavidgraham/masscan/blob/144c527ed55275ee9fbb80bb14fbb5e3fcff3b7e/src/proto-ssl.c#L1059).
+
+#Configuration
+
+#Usage
